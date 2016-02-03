db72069814c36a88dfc06f40f3edba405f212d64
Philip Newborough <corenominal@corenominal.org>
2016-02-03 18:49:54 +0000

    Set-up plugin

---
+++ internet.engineering-wp-api.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: internet.engineering WP API
+ * Plugin URI: https://github.com/corenominal/internet.engineering-wp-api
+ * Description: A WordPress plugin to provide extra functionality for the internet.engineering site.
+ * Author: Philip Newborough
+ * Version: 0.0.1
+ * Author URI: https://corenominal.org
+ */
