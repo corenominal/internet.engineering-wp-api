21ab8032c13f1dcc35c77fe2de41039e4cc677f5
Philip Newborough <corenominal@corenominal.org>
2016-02-08 07:56:15 +0000

    Clean-up tmp cruft

--- endpoints/get_role.php
+++ endpoints/get_role.php
@@ -4,13 +4,6 @@
  */
 function ie_api_get_role( $request_data )
 {
-	// Temp
-	if( isset( $_COOKIE['ie_account'] ) )
-	{
-		$data['ie_account'] = $_COOKIE['ie_account'];
-		return $data;
-	}
-
 	// Capture request data
 	$request = $request_data->get_params();
 
